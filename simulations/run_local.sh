f686d9b7c3e2542ba9a810db9afb486856af51d1
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2022-09-16 15:09:37 +1200

    Add much improved run_local from @jenrandrews

--- simulations/run_local.sh
+++ simulations/run_local.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Local paths
 DETECTION_HOSTPATH="${PWD}/detections"
 
@@ -5,13 +7,57 @@ DETECTION_HOSTPATH="${PWD}/detections"
 BASE_PATH="/tmp/outputs"
 DETECTION_DOCKERPATH="${BASE_PATH}/detections"
 
+if [ ! -d $DETECTION_HOSTPATH ];then
+  mkdir $DETECTION_HOSTPATH
+  chmod og+wx $DETECTION_HOSTPATH
+fi
+
 IMAGE="rteq-simulator"
+TAG="latest"
 
-docker build -t $IMAGE .
-# Usually you should be able to re-use the old image, for changes to the rteqcorrscan or eqcorrscan repos we need to rebuild
-# docker build --no-cache -t $IMAGE .
+function usage(){
+cat <<EOF
+Usage: $0 [Options] 
+Build or run docker $IMAGE
 
-docker run \
+Optional Arguments:
+    -h, --help              Show this message.
+    -b, --build             Rebuild the image.
+    -i, --interactive       Start the container with a bash prompt.
+    --run                   Run event 2014p051675
+    --image                 Provide alternative image name.
+    --tag                   Provide alternative tag
+EOF
+}
+
+# Processing command line options
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -b | --build) BUILD=true;;
+        -i | --interactive) INTERACTIVE=true;;
+        --run) RUN=true;shift;;
+        --image) IMAGE="$2";shift;;
+        --tag) TAG="$2";shift;;
+        -h) usage; exit 0;;
+        -*) usage; exit 1;;
+esac
+shift
+done
+
+if [ "${BUILD}" == "true" ]; then
+  echo "Removing current version of ${IMAGE}:${TAG}"
+  docker rmi "${IMAGE}:${TAG}"
+
+  echo "Building ${IMAGE}:${TAG}"
+  # Usually you should be able to re-use the old image, for changes to the rteqcorrscan or 
+  # eqcorrscan repos we need to rebuild
+  # docker build -t $IMAGE .
+  docker build --no-cache -t $IMAGE:${TAG} .
+fi
+
+if [ "${RUN}" == "true" ]; then
+  docker run \
     -m 16g --cpus=6 \
     -v $DETECTION_HOSTPATH:$DETECTION_DOCKERPATH \
     $IMAGE rteqcorrscan-simulation \
@@ -22,3 +68,14 @@ docker run \
     --client GEONET \
     --speed-up 10 \
     --working-dir $DETECTION_DOCKERPATH
+fi
+
+if [ "${INTERACTIVE}" == "true" ]; then
+  docker run -it --rm \
+      -m 16g --cpus=6 \
+      -v $DETECTION_HOSTPATH:$DETECTION_DOCKERPATH \
+      --entrypoint /bin/bash \
+      ${IMAGE}:${TAG} 
+  exit 0
+fi
+
